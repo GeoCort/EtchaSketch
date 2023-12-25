926e1dee81097d1edd1e777f718ac2e713ff3191
George <cortes.george93@icloud.com>
2023-12-25 11:53:35 -0800

    added sketch functions

--- script.js
+++ script.js
@@ -1,7 +1,27 @@
 let container = document.getElementById("sketch-c")
-for(let i = 0; i <16; i++){
-    let div = document.createElement("div", "class")
-    let msg = document.createTextNode(`${i}`)
-    div.classList.add("test")
-    container.appendChild(div);
+let grid_size = 64;
+function startBoard(){
+    for(let k =0; k<grid_size; k++){
+        let row = document.createElement("div");
+        row.classList.add("hori")
+        for(let i = 0; i <grid_size; i++){
+            let div = document.createElement("div", "class")
+            let msg = document.createTextNode(`${i}`)
+            div.classList.add("basic")
+            row.appendChild(div);
+        }
+        container.appendChild(row);
+    }
 }
+function clearContainer(){
+    while(container.firstChild){
+        container.removeChild(container.firstChild)
+    }
+}
+function resetGame(){
+    clearContainer();
+    startBoard();
+}
+
+startBoard();
+
