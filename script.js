4f86e66711fa014ea57933e9bf3f212c056c3e7c
George <cortes.george93@icloud.com>
2023-12-25 14:54:06 -0800

    finished grid

--- script.js
+++ script.js
@@ -1,27 +1,41 @@
-let container = document.getElementById("sketch-c")
-let grid_size = 64;
-function startBoard(){
+let container = document.getElementById("sketch-c") // grid container
+let range = document.getElementById("gridrange");
+range_display = document.getElementById("rangelabel")
+let grid_size = 40;
+//slider event handler
+
+function makeCell(grid_size){
+    let div= document.createElement("div");
+    div.style.width =`${grid_size}px`
+    div.style.height =`${grid_size}px`
+    div.classList.add("grid-style")
+    return div;
+}
+function startBoard(size){
+    container.style.gridTemplateColumns = `repeat(${grid_size},1fr)`
     for(let k =0; k<grid_size; k++){
-        let row = document.createElement("div");
-        row.classList.add("hori")
         for(let i = 0; i <grid_size; i++){
-            let div = document.createElement("div", "class")
-            let msg = document.createTextNode(`${i}`)
-            div.classList.add("basic")
-            row.appendChild(div);
+            container.appendChild(makeCell(size));
         }
-        container.appendChild(row);
     }
 }
 function clearContainer(){
-    while(container.firstChild){
-        container.removeChild(container.firstChild)
-    }
+    container.innerHTML=""
 }
-function resetGame(){
+function resetGame(s){
     clearContainer();
-    startBoard();
+    startBoard(s);
 }
 
-startBoard();
+startBoard(container.clientWidth/grid_size);
 
+range.addEventListener("input",()=>{
+    console.log(range.value)
+    rangelabel.textContent= `${range.value} x ${range.value}`;
+    console.log(grid_size)
+    console.log(typeof(grid_size))
+     grid_size =  parseInt(range.value)
+     console.log(grid_size)
+     console.log(typeof(grid_size))
+     resetGame(container.clientWidth/ grid_size)
+})
